92d520eec628ce2afd38bcba177e391c93626a59
Rytis Karpuška <rytis.karpuska@gmail.com>
2020-11-04 01:54:50 +0200

    Add value output to the register which outputs its value regardless of en signal

--- register.vhdl
+++ register.vhdl
@@ -3,27 +3,29 @@ use ieee.std_logic_1164.all;
 
 ENTITY cpu_register IS
 	port(
-		output : OUT std_logic_vector(7 downto 0);
+		output : OUT std_logic_vector(7 downto 0) := "ZZZZZZZZ";
+		value  : OUT std_logic_vector(7 downto 0) := "00000000";
 		input  : IN std_logic_vector(7 downto 0);
 		set    : IN std_logic;
-		clk    : IN std_logic;
 		en     : IN std_logic
 	);
 END ENTITY;
 
 ARCHITECTURE cpu_register OF cpu_register IS
-	SIGNAL value : std_logic_vector(7 downto 0) := "00000000";
+	SIGNAL inner_value : std_logic_vector(7 downto 0) := "00000000";
 BEGIN
-	step : PROCESS (clk, en)
+	step : PROCESS (en, set, input)
 	BEGIN
 		IF en = '1' THEN
-			output <= value;
+			output <= inner_value;
 		ELSE
 			output <= "ZZZZZZZZ";
 		END IF;
 
-		IF set = '1' AND rising_edge(clk) THEN
+		IF set = '1' THEN
+			inner_value <= input;
 			value <= input;
 		END IF;
+
 	END PROCESS;
 END;
