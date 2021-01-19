13fc969c0d9db45d2642bbd8cb94e47651cf84b0
Rytis Karpuška <rytis.karpuska@gmail.com>
2021-01-20 00:39:22 +0200

    Update mux

--- mux.vhdl
+++ mux.vhdl
@@ -6,28 +6,30 @@ ENTITY mux IS
 		in1 : IN std_logic_vector(7 downto 0) := "00000000";
 		in2 : IN std_logic_vector(7 downto 0) := "00000000";
 		in3 : IN std_logic_vector(7 downto 0) := "00000000";
-		in4 : IN std_logic_vector(7 downto 0) := "00000000";
 
 		en   : IN std_logic;
 		sel  : IN std_logic_vector(1 downto 0);
 
-		output : OUT std_logic_vector(7 downto 0);
+		output : OUT std_logic_vector(7 downto 0)
 	);
 END ENTITY;
 
 ARCHITECTURE mux OF mux IS
 BEGIN
-	step : PROCESS(in1, in2, in3, in4, en, sel)
+	step : PROCESS(in1, in2, in3, en, sel)
 	BEGIN
 		IF en = '1' THEN
-			WITH sel SELECT
-				output <= in1 WHEN "00",
-					  in2 WHEN "01",
-					  in3 WHEN "10",
-					  in4 WHEN "11";
-
+			IF sel = "00" THEN
+				output <= in1;
+			ELSIF sel = "01" THEN
+				output <= in2;
+			ELSIF sel = "10" THEN
+				output <= in3;
+			ELSE
+				output <= "00000000";
+			END IF;
 		ELSE
-			output <= "ZZZZZZ";
-		END IF
+			output <= "ZZZZZZZZ";
+		END IF;
 	END PROCESS;
 END ARCHITECTURE;
