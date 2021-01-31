b5d0169f98aff3d166a7edff4fa28af0bfdacee5
Rytis Karpuška <rytis.karpuska@gmail.com>
2021-01-31 23:38:38 +0200

    connect data_bus and addr_bus directly to CPU outputs for memory

--- cpu.vhdl
+++ cpu.vhdl
@@ -8,22 +8,20 @@ ENTITY cpu IS
 		reset : IN std_logic;
 
 		-- clock
-		clk: IN std_logic;
+		clk : IN std_logic;
 
 		-- Memory interface
-		mem_data : INOUT std_logic_vector(7 downto 0);
-		mem_addr : OUT std_logic_vector(7 downto 0);
 		mem_ce : OUT std_logic;
 		mem_oe : OUT std_logic;
-		mem_we : OUT std_logic
+		mem_we : OUT std_logic;
+
+		-- busses
+		data_bus : INOUT std_logic_vector(7 downto 0);
+		addr_bus : INOUT std_logic_vector(7 downto 0)
 	);
 END ENTITY;
 
 ARCHITECTURE cpu OF cpu IS
-	-- internal busses
-	SIGNAL addr_bus : std_logic_vector(7 downto 0);
-	SIGNAL data_bus : std_logic_vector(7 downto 0);
-
 	-- internal busses multiplexers control
 	SIGNAL data_mux_sel : std_logic_vector(1 downto 0);
 	SIGNAL data_mux_en  : std_logic;
@@ -66,7 +64,6 @@ BEGIN
 		in1 => reg_a_out,
 		in2 => reg_b_out,
 		in3 => alu_out,
-		in4 => mem_data,
 		en => data_mux_en,
 		sel => data_mux_sel,
 		output => data_bus);
@@ -75,7 +72,6 @@ BEGIN
 		in1 => reg_a_out,
 		in2 => reg_b_out,
 		in3 => alu_out,
-		in4 => (others => 'Z'), -- unused
 		en => addr_mux_en,
 		sel => addr_mux_sel,
 		output => addr_bus);
--- mux.vhdl
+++ mux.vhdl
@@ -6,7 +6,6 @@ ENTITY mux IS
 		in1 : IN std_logic_vector(7 downto 0);
 		in2 : IN std_logic_vector(7 downto 0);
 		in3 : IN std_logic_vector(7 downto 0);
-		in4 : IN std_logic_vector(7 downto 0);
 
 		en   : IN std_logic;
 		sel  : IN std_logic_vector(1 downto 0);
@@ -18,13 +17,12 @@ END ENTITY;
 ARCHITECTURE mux OF mux IS
 	SIGNAL intermediate : std_logic_vector(7 downto 0);
 BEGIN
-	PROCESS(en, sel, in1, in2, in3, in4)
+	PROCESS(en, sel, in1, in2, in3)
 	BEGIN
 		CASE sel IS
 			WHEN "00" => intermediate <= in1;
 			WHEN "01" => intermediate <= in2;
 			WHEN "10" => intermediate <= in3;
-			WHEN "11" => intermediate <= in4;
 			WHEN others => intermediate <= (others => '0');
 		END CASE;
 		CASE en IS
