ca30a9bdd2aa9ce493848140aa03cafa484b8e0b
Rytis Karpuška <rytis.karpuska@gmail.com>
2021-01-31 22:59:46 +0200

    Add some stupid reset logic. Note that decoder will be reworked into state machine

--- cpu.vhdl
+++ cpu.vhdl
@@ -5,49 +5,51 @@ use ieee.std_logic_unsigned.all;
 ENTITY cpu IS
 	port(
 		-- reset
-		reset : IN std_logic := '0';
+		reset : IN std_logic;
 
 		-- clock
-		clk: IN std_logic := '0';
+		clk: IN std_logic;
 
 		-- Memory interface
-		mem_data : INOUT std_logic_vector(7 downto 0) := (others => '0');
-		mem_addr : OUT std_logic_vector(7 downto 0) := (others => '0');
-		mem_ce : OUT std_logic := '0';
-		mem_oe : OUT std_logic := '0';
-		mem_we : OUT std_logic := '0'
+		mem_data : INOUT std_logic_vector(7 downto 0);
+		mem_addr : OUT std_logic_vector(7 downto 0);
+		mem_ce : OUT std_logic;
+		mem_oe : OUT std_logic;
+		mem_we : OUT std_logic
 	);
 END ENTITY;
 
 ARCHITECTURE cpu OF cpu IS
 	-- internal busses
-	SIGNAL addr_bus : std_logic_vector(7 downto 0) := (others => '0');
-	SIGNAL data_bus : std_logic_vector(7 downto 0) := (others => '0');
+	SIGNAL addr_bus : std_logic_vector(7 downto 0);
+	SIGNAL data_bus : std_logic_vector(7 downto 0);
 
 	-- internal busses multiplexers control
-	SIGNAL data_mux_sel : std_logic_vector(1 downto 0) := (others => '0');
-	SIGNAL data_mux_en  : std_logic := '0';
-	SIGNAL addr_mux_sel : std_logic_vector(1 downto 0) := (others => '0');
-	SIGNAL addr_mux_en  : std_logic := '0';
+	SIGNAL data_mux_sel : std_logic_vector(1 downto 0);
+	SIGNAL data_mux_en  : std_logic;
+	SIGNAL addr_mux_sel : std_logic_vector(1 downto 0);
+	SIGNAL addr_mux_en  : std_logic;
 
 	-- register signals
-	SIGNAL reg_a_we : std_logic := '0';
-	SIGNAL reg_a_out: std_logic_vector(7 downto 0) := (others => '0');
-	SIGNAL reg_b_we : std_logic := '0';
-	SIGNAL reg_b_out: std_logic_vector(7 downto 0) := (others => '0');
+	SIGNAL reg_a_we : std_logic;
+	SIGNAL reg_a_out: std_logic_vector(7 downto 0);
+	SIGNAL reg_b_we : std_logic;
+	SIGNAL reg_b_out: std_logic_vector(7 downto 0);
 
 	-- ALU outputs
-	SIGNAL alu_out    : std_logic_vector(7 downto 0) := (others => '0');
-	SIGNAL alu_zero   : std_logic := '0';
+	SIGNAL alu_out    : std_logic_vector(7 downto 0);
+	SIGNAL alu_zero   : std_logic;
 
 BEGIN
 	reg1 : ENTITY work.cpu_register port map(
+		reset => reset,
 		clk => clk,
 		we => reg_a_we,
 		data => data_bus,
 		output => reg_a_out);
 
 	reg2 : ENTITY work.cpu_register port map(
+		reset => reset,
 		clk => clk,
 		we => reg_b_we,
 		data => data_bus,
@@ -79,6 +81,7 @@ BEGIN
 		output => addr_bus);
 
 	decoder1 : ENTITY work.decoder port map(
+		reset => reset,
 		clk => clk,
 		data_bus => data_bus,
 		addr_bus => addr_bus,
--- register.vhdl
+++ register.vhdl
@@ -3,15 +3,23 @@ use ieee.std_logic_1164.all;
 
 ENTITY cpu_register IS
 	port(
+		reset  : IN std_logic;
 		clk    : IN std_logic;
 		we     : IN std_logic;
 		data   : IN std_logic_vector(7 downto 0);
-		output : OUT std_logic_vector(7 downto 0) := "00000000"
+		output : OUT std_logic_vector(7 downto 0)
 	);
 END ENTITY;
 
 ARCHITECTURE cpu_register OF cpu_register IS
 BEGIN
+	reset_register : PROCESS (reset)
+	BEGIN
+		IF RESET = '1' THEN
+			output <= (others => '0');
+		END IF;
+	END PROCESS;
+
 	step : PROCESS (clk)
 	BEGIN
 		IF rising_edge(clk) AND we = '1' THEN
