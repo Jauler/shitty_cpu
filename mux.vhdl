9387d585251f6956fd24b2eb26d125c8e15cd90b
Rytis Karpuška <rytis.karpuska@gmail.com>
2021-03-12 00:33:40 +0200

    Fix a few mistakes in mux implementation

--- mux.vhdl
+++ mux.vhdl
@@ -6,9 +6,14 @@ ENTITY mux IS
 		in1 : IN std_logic_vector(7 downto 0);
 		in2 : IN std_logic_vector(7 downto 0);
 		in3 : IN std_logic_vector(7 downto 0);
+		in4 : IN std_logic_vector(7 downto 0);
+		in5 : IN std_logic_vector(7 downto 0);
+		in6 : IN std_logic_vector(7 downto 0);
+		in7 : IN std_logic_vector(7 downto 0);
+		in8 : IN std_logic_vector(7 downto 0);
 
 		en   : IN std_logic;
-		sel  : IN std_logic_vector(1 downto 0);
+		sel  : IN std_logic_vector(2 downto 0);
 
 		output : OUT std_logic_vector(7 downto 0)
 	);
@@ -17,12 +22,17 @@ END ENTITY;
 ARCHITECTURE mux_arch OF mux IS
 	SIGNAL intermediate : std_logic_vector(7 downto 0);
 BEGIN
-	PROCESS(en, sel, in1, in2, in3)
+	PROCESS(en, sel, in1, in2, in3, in4, in5, in6, in7, in8, intermediate)
 	BEGIN
 		CASE sel IS
-			WHEN "00" => intermediate <= in1;
-			WHEN "01" => intermediate <= in2;
-			WHEN "10" => intermediate <= in3;
+			WHEN "000" => intermediate <= in1;
+			WHEN "001" => intermediate <= in2;
+			WHEN "010" => intermediate <= in3;
+			WHEN "011" => intermediate <= in4;
+			WHEN "100" => intermediate <= in5;
+			WHEN "101" => intermediate <= in6;
+			WHEN "110" => intermediate <= in7;
+			WHEN "111" => intermediate <= in8;
 			WHEN others => intermediate <= (others => '0');
 		END CASE;
 		CASE en IS
