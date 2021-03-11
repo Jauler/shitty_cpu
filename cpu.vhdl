2df443b46b6ffd6d91cd7f4aa980859e0728b8d6
Rytis Karpuška <rytis.karpuska@gmail.com>
2021-03-12 00:34:20 +0200

    Rewrite decoder

--- cpu.vhdl
+++ cpu.vhdl
@@ -22,13 +22,24 @@ ENTITY cpu IS
 END ENTITY;
 
 ARCHITECTURE cpu_arch OF cpu IS
+	-- inverted clock
+	SIGNAL inv_clock : std_logic;
+
 	-- internal busses multiplexers control
-	SIGNAL data_mux_sel : std_logic_vector(1 downto 0);
+	SIGNAL data_mux_sel : std_logic_vector(2 downto 0);
 	SIGNAL data_mux_en  : std_logic;
-	SIGNAL addr_mux_sel : std_logic_vector(1 downto 0);
+	SIGNAL addr_mux_sel : std_logic_vector(2 downto 0);
 	SIGNAL addr_mux_en  : std_logic;
 
 	-- register signals
+	SIGNAL reg_pc_we : std_logic;
+	SIGNAL reg_pc_out: std_logic_vector(7 downto 0);
+	SIGNAL reg_pcinc1_out : std_logic_vector(7 downto 0); -- PC value incremented by 1
+	SIGNAL reg_pcinc2_out : std_logic_vector(7 downto 0); -- PC value incremented by 2
+	SIGNAL reg_operand_we : std_logic;
+	SIGNAL reg_operand_out: std_logic_vector(7 downto 0);
+	SIGNAL reg_instruction_we : std_logic;
+	SIGNAL reg_instruction_out: std_logic_vector(7 downto 0);
 	SIGNAL reg_a_we : std_logic;
 	SIGNAL reg_a_out: std_logic_vector(7 downto 0);
 	SIGNAL reg_b_we : std_logic;
@@ -39,6 +50,27 @@ ARCHITECTURE cpu_arch OF cpu IS
 	SIGNAL alu_zero   : std_logic;
 
 BEGIN
+	pc_reg1 : ENTITY work.cpu_register port map(
+		reset => reset,
+		clk => clk,
+		we => reg_pc_we,
+		data => data_bus,
+		output => reg_pc_out);
+
+	operand_reg1 : ENTITY work.cpu_register port map(
+		reset => reset,
+		clk => clk,
+		we => reg_operand_we,
+		data => data_bus,
+		output => reg_operand_out);
+
+	instruction_reg1 : ENTITY work.cpu_register port map(
+		reset => reset,
+		clk => clk,
+		we => reg_instruction_we,
+		data => data_bus,
+		output => reg_instruction_out);
+
 	reg1 : ENTITY work.cpu_register port map(
 		reset => reset,
 		clk => clk,
@@ -64,6 +96,11 @@ BEGIN
 		in1 => reg_a_out,
 		in2 => reg_b_out,
 		in3 => alu_out,
+		in4 => reg_operand_out,
+		in5 => reg_pc_out,
+		in6 => reg_pcinc1_out,
+		in7 => reg_pcinc2_out,
+		in8 => (others => '0'), -- unused
 		en => data_mux_en,
 		sel => data_mux_sel,
 		output => data_bus);
@@ -72,24 +109,45 @@ BEGIN
 		in1 => reg_a_out,
 		in2 => reg_b_out,
 		in3 => alu_out,
+		in4 => reg_operand_out,
+		in5 => reg_pc_out,
+		in6 => reg_pcinc1_out,
+		in7 => reg_pcinc2_out,
+		in8 => (others => '0'), -- unused
 		en => addr_mux_en,
 		sel => addr_mux_sel,
 		output => addr_bus);
 
 	decoder1 : ENTITY work.decoder port map(
 		reset => reset,
-		clk => clk,
-		data_bus => data_bus,
-		addr_bus => addr_bus,
+		clk => inv_clock,
+
+		-- memory and busses
 		mem_ce => mem_ce,
 		mem_oe => mem_oe,
 		mem_we => mem_we,
+
+		-- register control
+		reg_pc_we => reg_pc_we,
+		reg_instruction_we => reg_instruction_we,
+		reg_operand_we => reg_operand_we,
 		reg_a_we => reg_a_we,
 		reg_b_we => reg_b_we,
+
+		-- instruction register
+		instruction => reg_instruction_out,
+
+		-- alu
 		alu_zero => alu_zero,
+
+		-- muxes
 		data_mux_sel => data_mux_sel,
 		data_mux_en => data_mux_en,
 		addr_mux_sel => addr_mux_sel,
 		addr_mux_en => addr_mux_en);
+
+	reg_pcinc1_out <= reg_pc_out + 1;
+	reg_pcinc2_out <= reg_pc_out + 2;
+	inv_clock <= not clk;
 END ARCHITECTURE;
 
--- decoder.vhdl
+++ decoder.vhdl
@@ -5,14 +5,10 @@ use ieee.std_logic_unsigned.all;
 ENTITY decoder IS
 	port(
 		-- reset
-		reset : in std_logic;
+		reset : IN std_logic;
 
 		-- clock
 		clk : IN std_logic;
-
-		-- busses
-		data_bus : INOUT std_logic_vector(7 downto 0);
-		addr_bus : INOUT std_logic_vector(7 downto 0);
 
 		-- memory control
 		mem_ce : OUT std_logic;
@@ -20,109 +16,180 @@ ENTITY decoder IS
 		mem_we : OUT std_logic;
 
 		-- register control
+		reg_pc_we : OUT std_logic;
+		reg_operand_we : OUT std_logic;
+		reg_instruction_we : OUT std_logic;
 		reg_a_we : OUT std_logic;
 		reg_b_we : OUT std_logic;
+
+		-- instruction register value
+		instruction : IN std_logic_vector(7 downto 0);
 
 		-- ALU
 		alu_zero : IN std_logic;
 
 		-- muxes control
-		data_mux_sel : IN std_logic_vector(1 downto 0);
-		data_mux_en : IN std_logic;
-		addr_mux_sel : IN std_logic_vector(1 downto 0);
-		addr_mux_en : IN std_logic
+		data_mux_sel : OUT std_logic_vector(2 downto 0);
+		data_mux_en : OUT std_logic;
+		addr_mux_sel : OUT std_logic_vector(2 downto 0);
+		addr_mux_en : OUT std_logic
 	);
 END ENTITY;
 
 ARCHITECTURE decoder_arch OF decoder IS
-	SIGNAL subinstr : std_logic_vector(4 downto 0);
-	SIGNAL program_counter : std_logic_vector(7 downto 0);
-	SIGNAL instruction_l : std_logic_vector(7 downto 0);
-	SIGNAL instruction_h : std_logic_vector(7 downto 0);
+	TYPE states IS (FETCH_I, FETCH_O, DECODE, EXECUTE, INCREMENT);
+
+	SIGNAL state : States;
 BEGIN
-	step : PROCESS (clk)
+	PROCESS (clk, reset)
 	BEGIN
-		-- TODO: refactor into proper state machine
-		IF reset = '0' THEN
-			IF subinstr = 0 THEN
-				addr_bus <= program_counter;
-				mem_ce <= '1';
-				mem_oe <= '1';
-			END IF;
-			IF subinstr = 1 THEN
-				instruction_l <= data_bus;
-			END IF;
-			IF subinstr = 2 THEN
-				addr_bus <= program_counter + 1;
-			END IF;
-			IF subinstr = 3 THEN
-				instruction_h <= data_bus;
-			END IF;
-			IF subinstr = 4 THEN
-				addr_bus <= "ZZZZZZZZ";
-				mem_ce <= '0';
-				mem_oe <= '0';
-			END IF;
-
-
-
-
-			-- move data from instruction_h to reg a
-			IF instruction_l = "00000001" AND subinstr = 4 THEN
-				data_bus <= instruction_h;
-				reg_a_we <= '1';
-			END IF;
-			IF instruction_l = "00000001" AND subinstr = 5 THEN
-				data_bus <= "ZZZZZZZZ";
-				reg_a_we <= '0';
-			END IF;
-
-
-
-
-			-- move data from instruction_h to reg b
-			IF instruction_l = "00000010" AND subinstr = 4 THEN
-				data_bus <= instruction_h;
-				reg_b_we <= '1';
-			END IF;
-			IF instruction_l = "00000010" AND subinstr = 5 THEN
-				data_bus <= "ZZZZZZZZ";
-				reg_b_we <= '0';
-			END IF;
-
-
-
-
-			-- jmp instruction
-			IF instruction_l = "00000011" AND subinstr = 4 THEN
-				program_counter <= instruction_h;
-			END IF;
-
-
-
-			-- increment program counter if not jumped
-			IF instruction_l /= "00000011" AND subinstr = "1111" THEN
-				program_counter <= program_counter + 2;
-			END IF;
-
-			-- Track subinstruction
-			subinstr <= subinstr + 1;
-		ELSE
-			-- reset logic
-			subinstr <= (others => '0');
-			program_counter <= (others => '0');
-			instruction_l <= (others => '0');
-			instruction_h <= (others => '0');
-
-			data_bus <= (others => 'Z');
-			addr_bus <= (others => 'Z');
-
+		IF (reset = '1') THEN
+			state <= FETCH_I;
 			mem_ce <= '0';
 			mem_oe <= '0';
 			mem_we <= '0';
 
+			reg_pc_we <= '0';
+			reg_operand_we <= '0';
+			reg_instruction_we <= '0';
 			reg_a_we <= '0';
 			reg_b_we <= '0';
+
+			data_mux_en <= '0';
+			data_mux_sel <= "000";
+			addr_mux_en <= '0';
+			addr_mux_sel <= "000";
+
+		-- Now we set all signals on falling edge
+		-- Therefore the signals will be stable on
+		-- rising edge. Where most of the components
+		-- make their actions.
+		ELSIF (rising_edge(clk)) THEN
+			CASE state IS
+
+			-- read instruction byte into register
+			WHEN FETCH_I =>
+				-- clear signals from increment stage
+				data_mux_en <= '0';
+				reg_pc_we <= '0';
+
+				-- configure memory to read instruction byte
+				mem_ce <= '1';
+				mem_oe <= '1';
+				mem_we <= '0';
+				addr_mux_sel <= "100"; -- PC into addr bus
+				addr_mux_en <= '1';
+				reg_instruction_we <= '1';
+				state <= FETCH_O;
+
+			-- All instructions contains operand
+			-- byte... Even if it is not used :D
+			-- Lets setup memory, to read that
+			-- byte
+			WHEN FETCH_O =>
+				reg_instruction_we <= '0';
+
+				mem_ce <= '1';
+				mem_oe <= '1';
+				mem_we <= '0';
+				addr_mux_sel <= "101"; -- PC+1 into addr bus
+				addr_mux_en <= '1';
+				reg_operand_we <= '1';
+				state <= DECODE;
+
+			WHEN DECODE =>
+				reg_operand_we <= '0';
+				mem_ce <= '0';
+				mem_oe <= '0';
+				mem_we <= '0';
+				addr_mux_en <= '0';
+				reg_operand_we <= '0';
+
+				-- setup data according to instruction type
+				CASE instruction (7 downto 6) IS
+
+				-- direct (aka register to register) instruction
+				WHEN "00" =>
+					data_mux_en <= '1';
+					data_mux_sel <= instruction (2 downto 0);
+
+				-- memory to register instruction
+				WHEN "01" =>
+					mem_ce <= '1';
+					mem_oe <= '1';
+					mem_we <= '0';
+					addr_mux_sel <= instruction (2 downto 0);
+					addr_mux_en <= '1';
+
+				-- register to memory instruction
+				WHEN "10" =>
+					data_mux_sel <= instruction (2 downto 0);
+					data_mux_en <= '1';
+					addr_mux_sel <= instruction (5 downto 3);
+					addr_mux_en <= '1';
+
+				-- conditional jump instruction
+				WHEN "11" =>
+					IF alu_zero = '1' THEN
+						data_mux_sel <= "011";
+						data_mux_en <= '1';
+					END IF;
+
+				WHEN others =>
+				END CASE;
+
+				state <= EXECUTE;
+
+			WHEN EXECUTE =>
+				-- Data bus value has been configured in decode
+				-- stage. Now we need to configure write of this
+				-- data.
+
+				-- setup data according to instruction type
+				CASE instruction (7 downto 6) IS
+				WHEN "00" =>
+					reg_pc_we <= instruction(5);
+					reg_b_we <= instruction(4);
+					reg_a_we <= instruction(3);
+
+				WHEN "01" =>
+					reg_pc_we <= instruction(5);
+					reg_b_we <= instruction(4);
+					reg_a_we <= instruction(3);
+
+				WHEN "10" =>
+					mem_ce <= '1';
+					mem_oe <= '0';
+					mem_we <= '1';
+
+				WHEN "11" =>
+					IF alu_zero = '1' THEN
+						reg_pc_we <= '1';
+					END IF;
+
+				WHEN others =>
+				END CASE;
+
+				state <= INCREMENT;
+
+			WHEN INCREMENT =>
+				-- Clear all control signals
+				mem_ce <= '0';
+				mem_oe <= '0';
+				mem_we <= '0';
+				reg_operand_we <= '0';
+				reg_instruction_we <= '0';
+				reg_a_we <= '0';
+				reg_b_we <= '0';
+				addr_mux_en <= '0';
+				addr_mux_sel <= "000";
+
+				-- Increment PC by 2
+				data_mux_sel <= "110";
+				data_mux_en <= '1';
+				reg_pc_we <= '1';
+				state <= FETCH_I;
+			END CASE;
 		END IF;
 	END PROCESS;
 END;
